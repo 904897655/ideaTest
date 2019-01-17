4c4c7f67e213d1a1a7707b6cd1dcf6b0938354b7
心系蝶相印 <904897655@qq.com>
2019-01-17 18:25:57 +0800

    bendixiugai

---
+++ TestModel/src/DebugTest.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+/**
+ * @author 心系蝶相印 Onedirection
+ * @create 2019-01-17 18:18
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+
+    }
+}
